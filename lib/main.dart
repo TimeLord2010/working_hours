34e017d18f8faa56a0f0945b9ba905799e72c666
Vinicius Gabriel <timelord1025@gmail.com>
2022-05-02 10:49:00 -0300

    changed background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:work_hours_tracking/ui/main_page/main_page.dart';
 
 void main() {
-  debugRepaintRainbowEnabled = true;
+  //debugRepaintRainbowEnabled = true;
   runApp(const MyApp());
 }
 
@@ -11,10 +10,12 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    const bc = 240;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: const Color.fromARGB(255, bc, bc, bc),
       ),
       home: const MainPage(),
     );
