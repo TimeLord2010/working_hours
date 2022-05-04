f5d77dccf2d3026095da5dfb161485ac40bcd278
Vinicius Gabriel <timelord1025@gmail.com>
2022-05-03 23:18:27 -0300

    feat: passes interval provider

--- lib/ui/history/history_record/history_record_provider.dart
+++ lib/ui/history/history_record/history_record_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:work_hours_tracking/models/interval.dart' as im;
 import 'package:work_hours_tracking/ui/history/history_record/edit_history_record/edit_history_record.dart';
 import 'package:work_hours_tracking/ui/history/history_record/edit_history_record/edit_history_record_provider.dart';
+import 'package:work_hours_tracking/ui/providers/interval_provider.dart';
 
 class HistoryRecordProvider with ChangeNotifier {
   HistoryRecordProvider({
@@ -15,14 +17,18 @@ class HistoryRecordProvider with ChangeNotifier {
   final void Function() onDelete;
 
   void onTap() async {
+    final intervalProvider = context.read<IntervalProvider>();
     await showModalBottomSheet(
       context: context,
       builder: (context) {
-        return EditHistoryRecord.create((c) => EditHistoryRecordProvider(
-              context: c,
-              interval: interval,
-              onDelete: onDelete,
-            ));
+        return ChangeNotifierProvider.value(
+          value: intervalProvider,
+          child: EditHistoryRecord.create((c) => EditHistoryRecordProvider(
+                context: c,
+                interval: interval,
+                onDelete: onDelete,
+              )),
+        );
       },
     );
   }
